d255c7d99029fcce7f0d15c05d77823eae02ae3e
Murielly-Nascimento <80076673+Murielly-Nascimento@users.noreply.github.com>
2021-07-27 18:27:31 -0300

    Add files via upload

---
+++ Projetos/Aula 5/src/TestaVetores.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TestaVetores {
+
+	public static void main(String[] args) {
+		int c[] = {1,2,3,4,5,6};
+		int b[] = new int[10];
+		
+		Scanner entrada = new Scanner(System.in);
+		for(int i =0; i<c.length;i++) {
+			System.out.println("Entre com o " + (i+1) + " o elemnto");
+			c[i] = entrada.nextInt();
+		}
+		
+		for(int elem: c) {
+			System.out.println(elem);
+		}
+
+	}
+
+}
